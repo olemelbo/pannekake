5aa4343436c594663d2d2f199ab23c70f3496bf9
Ole Christian Melbostad <olemelbo1@gmail.com>
2013-04-04 20:33:38 +0200

    Compile error rettet opp

--- reservasjon.cpp
+++ reservasjon.cpp
@@ -45,8 +45,7 @@ void Reservasjon::les_fra_fil() {
 
 void Reservasjon::skriv_til_fil(ostream* ut) {
 	*ut << avreise_dato << "\n"
-		<< antall_dogn << "\n"
-		<< 
+		<< antall_dogn << "\n";
 }
 
 void Reservasjon::display() {
